c79d48162ef8bedddf1a3326bb96a80ff0b59388
Andres Torre <a.torre.gonz@gmail.com>
2016-09-18 19:21:37 +0100

    error fixed

--- golosinasPitufos/src/main/java/com/programmingfree/springservice/PricesRepository.java
+++ golosinasPitufos/src/main/java/com/programmingfree/springservice/PricesRepository.java
@@ -7,8 +7,8 @@ import java.util.List;
 /**
  * Created by pin69 on 18-Sep-16.
  */
-public interface PricesRepository extends CrudRepository<Provider, Integer> {
+public interface PricesRepository extends CrudRepository<Prices, Integer> {
 
-    List<Prices> findByProviderArchived();
+    //List<Prices> findByProviderArchived();
 }
 
